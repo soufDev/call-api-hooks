4e4537ff245e508a15a3453aa3458e64b2653239
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2018-11-30 00:19:09 +0100

    :art: deal with modal style for better behave

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Modal.css';
 
 interface Props {
-  open?: boolean;
+  open: boolean;
   title?: string;
   content?: JSX.Element;
   onClose: () => void;
 }
 
+const initialValue = 'auto';
 export default function Modal(props: Props) {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => document.body.style.overflow = initialValue;
+  })
   return (
     <div id="myModal" className="modal" style={props.open && { display: 'block' } || { display: 'none' }}>
       <div className="modal-content">
